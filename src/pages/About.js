fb99860c16eba005b1a9f3621bdc348f2df412b7
Joao Carneiro <jvmcarneiro@gmail.com>
2021-04-03 22:14:45 -0300

    Melhora página de Sobre

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,9 +4,11 @@ export const About = () => (
   <div class="Wrapper">
     <h2>Sobre o site do LaR - Laboratório de Robótica da UFBA</h2>
     <div class="abouttext">
-      <p>Este site foi construído para prototipar o acesso virtual e remoto ao LaR.</p>
-      <p>Aqui é possivel alunos cadastrados realizarem experimentos e simulações de forma virtual ou remota, utilizando as dependências e componentes do Laboratório presentes no DEEC, no 4° andar da EPUFBA.</p>
-      <p>Estão presentes também links e referências para conteúdos relativos ao tema de sistemas de controle.</p>
+      <p>Este site foi construído para prototipar o acesso virtual e remoto ao LaR.</p><br />
+      <br />
+      <p>Aqui é possivel alunos cadastrados realizarem experimentos e simulações de forma virtual ou remota, utilizando as dependências e componentes do Laboratório presentes no DEEC, no 4° andar da EPUFBA.</p><br />
+      <p>Estão presentes também links e referências para conteúdos relativos ao tema de sistemas de controle.</p><br />
+      <br />
       <p>Contato: <a href="http://www.eng.ufba.br/laboratorio-de-robotica-lar">http://www.eng.ufba.br/laboratorio-de-robotica-lar</a></p>
     </div>
   </div>
